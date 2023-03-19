beac87ef66816b00c305c8687ff97061d0f2bafe
Cassandra <kassy.loulou61@gmail.com>
2023-03-19 15:12:27 +0100

    J'ai ajouter le js pour bouton suppression tâches terminé + html et un peu de css

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const myTask = document.querySelector(".sous-tache");
 const deleteBtn = document.querySelector(".gg-close");
 
 deleteBtn.addEventListener("click", function () {
-  myTask.remove ();
-  deleteBtn.remove ();
+	myTask.remove();
+	deleteBtn.remove();
+});
+// Bouton supprimer
+const supprimer = document.querySelector(".sup");
+const enlever = document.querySelector(".supp");
+const confirmer = document.querySelector(".confirmer");
+
+confirmer.addEventListener("click", function () {
+	let = confirm("Êtes-vous sûr de vouloir supprimer?");
+	supprimer.remove();
+	enlever.remove();
 });
